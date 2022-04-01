c8e1a8d94e0d68ac1418d3cce100757a8a782693
Edvaldo Afonso <eadafonso@gmail.com>
2022-04-01 13:36:05 +0100

    chore: configure theme and next seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
+import { NextSeo } from "next-seo";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
 import Global from "../styles/Global";
+import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Global />
-      <Component {...pageProps} />)
-    </>
+      <NextSeo title="EadBox | Dashboard" description="EadBox Dashboard" />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,50 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  border: {
+    radius: "0.4rem",
+  },
+  font: {
+    family: "Poppins, sans-serif",
+    title: "Poppins",
+    bold: 500,
+    light: 200,
+  },
+  sizes: {
+    xxsmall: "0.9rem",
+    xsmall: "1.6rem",
+    small: "1.8rem",
+    medium: "2.2rem",
+    large: "2.6rem",
+    xlarge: "3.4rem",
+    xxlarge: "5.2rem",
+  },
+  colors: {
+    black: "#1c1c1c",
+    black_100: "#141414",
+    white: "#fff",
+
+    gray_50: "#f7f8fa",
+    gray_100: "#e6e8eb",
+    gray_200: "#afb2b1",
+    gray_500: "#808080",
+    gray_800: "#494d4b",
+
+    background: "#f1f5f9",
+    gray_900: "#e4edff",
+
+    green: "#1ea462",
+    blue: "#4486f4",
+    red: "#ff4a56",
+    yellow: "#ffd04c",
+    purple: "#7602c2",
+  },
+  spacings: {
+    xxsmall: "0.8rem",
+    xsmall: "1.6rem",
+    small: "2.4rem",
+    medium: "3.2rem",
+    large: "6.0rem",
+    xlarge: "6.4rem",
+    xxlarge: "12.8rem",
+  },
+};
